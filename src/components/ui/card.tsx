b5b48e02791caff8d7ee5d815ae68f4aa2c4bd94
STBN2023 <stbn2023@gmail.com>
2025-02-20 16:37:12 +0100

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,22 +1,22 @@
 "use client"
-
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+// Supprimez l'interface vide et utilisez directement le type de base
+const Card = React.forwardRef
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      className
+    )}
+    {...props}
+  />
+))
 
-const Card = React.forwardRef<HTMLDivElement, CardProps>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn(
-        "rounded-lg border bg-card text-card-foreground shadow-sm",
-        className
-      )}
-      {...props}
-    />
-  )
-)
 Card.displayName = "Card"
 
 export { Card }

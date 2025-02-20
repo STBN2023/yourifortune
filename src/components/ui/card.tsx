7382f82783bee5a14b0118d67de9bae420ecab2d
STBN2023 <stbn2023@gmail.com>
2025-02-20 16:46:41 +0100

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,7 +2,6 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-// Supprimez l'interface vide et utilisez directement le type de base
 const Card = React.forwardRef
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
